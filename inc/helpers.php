99bb2478912d29a12d13d2316a2181ae833725b0
Mike.tropi <mike.beplus@gmail.com>
2021-10-15 11:36:33 +0700

    Validate qty add_to_cart & update_cart => Done

--- inc/helpers.php
+++ inc/helpers.php
@@ -67,6 +67,59 @@ function slwc_get_all_term_product_locations() {
   }
 }
 
+function slwc_find_item_in_cart($product_id = 0) {
+  global $woocommerce;
+  $cart_items = $woocommerce->cart->get_cart();
+  if(count($cart_items) == 0 || $product_id == 0) return false;
+
+  # Product type variation
+  $key = array_search($product_id, array_column($cart_items, 'variation_id'));
+  
+  if($key !== false) {
+    return $cart_items[array_keys($cart_items)[$key]];
+  }
+
+  # Product type single
+  $key = array_search($product_id, array_column($cart_items, 'product_id'));
+  if($key !== false) {
+    return $cart_items[array_keys($cart_items)[$key]];
+  }
+
+  return false;
+}
+
+/**
+ * Qty validate before add_to_cart or update cart
+ * 
+ * @param Int $product_id
+ * @param Int $qty
+ * @param Int $stock_location
+ * @param Boolean $replace_qty
+ * 
+ * @return Boolean true/false
+ */
+function slwc_qty_validation($product_id = 0, $qty = 1, $stock_location = 0, $replace_qty = false) {
+  if(empty($product_id) || empty($qty) || empty($stock_location)) return false;
+
+  # Get all stock location by product
+  $product_stock = SlwStockAllocationHelper::getProductStockLocations($product_id);
+
+  # Store selected
+  $location_selected = $product_stock[$stock_location];
+  
+  # Totally qty of store
+  $qty_totally_location = (int) $location_selected->quantity;
+
+  # Product exist in cart 
+  $produt_in_cart = slwc_find_item_in_cart($product_id);
+  
+  if($produt_in_cart && $replace_qty == false) {
+    $qty += (int) $produt_in_cart['quantity'];
+  }
+
+  return ($qty > $qty_totally_location) ? false : true;
+}
+
 add_action('init', function() {
   // echo '<pre>';
   // slwc_country_code_options();
--- inc/hooks.php
+++ inc/hooks.php
@@ -1,14 +1,81 @@
 <?php 
+use SLW\SRC\Helpers\SlwStockAllocationHelper;
 /**
  * Hooks 
  */
 
 function slwc_woo_add_to_cart_validation($true, $product_id, $quantity) {
-  // print_r($_POST); die;
-  // $product_id = if($_POST['variation_id']) ? $_POST['variation_id'] : $_POST['product_id']; 
-  // $product_stock = SlwStockAllocationHelper::getProductStockLocations($product_id);
-  // return false;
-  return true;
+  if(isset($_POST['add-to-cart']) || isset($_POST['variation_id']) || isset($_POST['slw_add_to_cart_item_stock_location'])) {
+    # get product single or variation 
+    $product_id = ($_POST['variation_id']) ? (int) $_POST['variation_id'] : (int) $_POST['add-to-cart']; 
+    
+    # Client Qty
+    $qty = isset($_POST['quantity']) ? (int) $_POST['quantity'] : 1;
+
+    # Stock location 
+    $stock_location = $_POST['slw_add_to_cart_item_stock_location'];
+
+    # validate
+    $pass = slwc_qty_validation($product_id, $qty, $stock_location);
+    
+    if($pass === false) {
+      # Get all stock location by product
+      $product_stock = SlwStockAllocationHelper::getProductStockLocations($product_id);
+
+      # Store selected
+      $location_selected = $product_stock[$stock_location];
+
+      $error_message = sprintf("%s (%s).", __('Your order quantity exceeds the quantity available in store', 'slwc'), "<u>$location_selected->name</u> available $location_selected->quantity in total");
+      wc_add_notice($error_message, 'error');
+
+      return false;
+    }
+  }
+
+  return $true;
 }
 
-add_filter( 'woocommerce_add_to_cart_validation', 'slwc_woo_add_to_cart_validation', 20, 3 );
+add_filter('woocommerce_add_to_cart_validation', 'slwc_woo_add_to_cart_validation', 99, 3);
+
+function slwc_woo_update_cart_validation($true, $cart_item_key, $values, $quantity) {
+  // echo '<pre>'; print_r([$true, $cart_item_key, $values, $quantity]); echo '</pre>';
+  // die;
+
+  # get product single or variation 
+  $product_id = ($values['variation_id']) ? (int) $values['variation_id'] : (int) $values['product_id']; 
+
+  # Update Qty
+  $qty = $quantity;
+
+  # Stock location 
+  $stock_location = $values['stock_location'];
+
+  # validate
+  $pass = slwc_qty_validation($product_id, $qty, $stock_location, true);
+
+  if($pass === false) {
+    # Product
+    $_product = wc_get_product($product_id); 
+
+    # Get all stock location by product
+    $product_stock = SlwStockAllocationHelper::getProductStockLocations($product_id);
+
+    # Store selected
+    $location_selected = $product_stock[$stock_location];
+
+    # Error message
+    $error_message = sprintf( 
+        "%s, %s (%s).", 
+        '<a href="'. get_permalink($_product->get_id()) .'" target="_blank">(#'. $_product->get_id() .') '. $_product->get_name() .'</a>', 
+        __('your order quantity exceeds the quantity available in store', 'slwc'), 
+        "<u>{$location_selected->name}</u> available {$location_selected->quantity} in total"
+      );
+
+    wc_add_notice($error_message, 'error');
+    return false;
+  }
+
+  return $true;
+}
+
+add_filter('woocommerce_update_cart_validation', 'slwc_woo_update_cart_validation', 99, 4);
